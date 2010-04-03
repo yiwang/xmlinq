ccac7d4393e15a0549d87abad1ce0c8b78a6996a
U-E\xp <xp@e.(none)>
2010-04-03 16:09:07 -0400

    createXElem

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Linq;
+// A "Hello World"
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("HW");
+            createXElem();
+            //createXDoc();
+            Console.ReadLine();
+        }
+        private static void createXDoc()
+        {
+            XDocument doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes")
+            );
+        }
+        private static void createXElem() {
+            XElement xml = new XElement("contacts",
+                                new XElement("contact",
+                                    new XAttribute("contactID", "2"),
+                                    new XElement("firstName", "Barry")
+                                ),
+                                new XElement("contact",
+                                    new XAttribute("contactID", "3"),
+                                    new XAttribute("ff", "BB")
+                                )
+                           );
+            Console.WriteLine(xml);
+        }
+    }
+}
