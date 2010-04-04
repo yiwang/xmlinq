99207ad9c4aebc65748591c19c4fe525a5391208
U-E\xp <xp@e.(none)>
2010-04-03 21:35:53 -0400

    query on double[]

--- LinqQuery.cs
+++ LinqQuery.cs
@@ -15,7 +15,22 @@ namespace XmLinq
             //Linq2XML.Main0();
             //AlterXML.Main1(args);
 
+            double[] nums = new double[] { 0, 4, 2, 3, 543, 3343.3, 432, -3, 0 };
+            var result = from n in nums
+                         //where n < 5
+                         orderby n                         
+                         select n;
+
+            var arr = result.ToArray().Reverse();
+            foreach (double e in arr)
+            {
+                Console.Write(e);
+                Console.Write(", ");
+            }
+            Console.WriteLine(arr);
+            
             Console.ReadLine();
         }
+
     }
 }
